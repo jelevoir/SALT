2e3ffce2e88a8694ff0431835e0aded43214c72a
jelevoir <jelevoir@oakland.edu>
2016-07-27 22:17:48 -0400

    Insert Sentiments into DB. Prints all Sentiments 0 or less.

--- main.go
+++ main.go
@@ -17,24 +17,41 @@ func main() {
 
     // Implements location specific tweet
     v := url.Values{}
-    v.Add("geocode","42.6725469,-83.2170434,5mi")
+    //Lookup geocodes at geocoder.us
+    v.Add("geocode","42.6725469,-83.2170434,20mi")
     v.Add("lang","en")
-	searchResult, _ := api.GetSearch("OU", v)
+	searchResult, _ := api.GetSearch("#StrangerThings", v)
 
-    // Get API key
+    // Get Google Natural Language API Key
     key := GetAPIKey()
+
+    var sentiments []Sentiments
 
     for _, tweet := range searchResult.Statuses {
         sentiment := GetSentiment(key, tweet.Text)
-        fmt.Println(tweet.Text)
-        fmt.Println(sentiment.Sentiment.Polarity)
+        sentiments = append(sentiments, Sentiments{0, sentiment.Sentiment.Polarity, sentiment.Sentiment.Magnitude, tweet.User.ScreenName})
     }
+
+    // Insert Sentiments into database
+    err = InsertSentiment(db, sentiments)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    // Insert Tweets into database
 	err = InsertTweet(db, searchResult.Statuses)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	var words []Word
-	words = append(words, Word{0, "the", 0, 1})
-	InsertWord(db, words)
+    // Print Negative Sentiments
+    negs, err := GetNegativeSentiments(db)
+    if err != nil {
+        fmt.Println(err)
+    }
+    for i, neg := range negs {
+        fmt.Println("Tweet Number: ", i)
+        fmt.Printf("Author: %v\nSentiment: %v \nData: %v\n\n", neg.Author, neg.Sentiment, neg.Data)
+    }
+
 }
